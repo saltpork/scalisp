f85912184ec6f339a5653f736619a5bcb6815eaa
Salt Pork <swadeless@gmail.com>
2014-01-02 22:54:11 -0500

    - updates

--- src/scalisp.scala
+++ src/scalisp.scala
@@ -1,15 +1,72 @@
-#!/bin/bash
-# -*- mode: scala -*-
-exec env scala -deprecation "$0" "$@"
-!#
-
-//package scalisp
+package scalisp
 
 import util.parsing.combinator.RegexParsers
 import collection.mutable.{Map => mMap}
 
-//object `package` {
-//}
+object `package` {
+  // Eval
+  def eval(expr : Any, scope : Scope) : Any = expr match {
+      case Symbol(name) => scope(name)
+      case args : List[Any] =>
+        args match {
+          case Symbol("lambda") :: (fargs : List[Any]) :: body =>
+            val symargs = fargs.map { x => assert(x.isInstanceOf[Symbol], s"lambda argument $x is not a string!"); x.asInstanceOf[Symbol] }
+
+            { (xs : List[Any]) =>
+              val newScope = Scope(scope, newBindings = (symargs.map(_.name) zip xs) : _*)
+              for (l <- body.dropRight(1)) eval(l, newScope)
+              eval(body.last, newScope)
+            }
+          case Symbol("lambda") :: rest => throw Error(s"malformed lambda expression (lambda ${rest})")
+          case Symbol("quote") :: rest :: Nil => rest // TODO: may be a macro
+          case (Symbol("set!") | Symbol("def")) :: Symbol(name) :: value :: Nil =>
+            val res = eval(value, scope)
+            scope += name -> res
+            res
+          case Symbol("macro") :: Symbol(name) :: (margs : List[Any]) :: body =>
+            val symargs = margs.map { x => assert(x.isInstanceOf[Symbol], s"macro argument $x is not a string!"); x.asInstanceOf[Symbol] }
+
+            val mac = Macro({ (xs : List[Any]) =>
+                              val newScope = Scope(scope, newBindings = (symargs.map(_.name) zip xs) : _*)
+                              for (l <- body.dropRight(1)) eval(l, newScope)
+                              val x = eval(body.last, newScope)
+                              x
+                            })
+            scope += name -> mac
+            mac
+          case Symbol(fn) :: rest => // Function call
+            if (scope isDefinedAt fn) {
+              scope(fn) match {
+                case Macro(mac) =>
+                  val expr = mac(rest)
+                  eval(expr, scope)
+                case func @ _ =>
+                  val evalArgs = rest.map(e => eval(e, scope))
+                func.asInstanceOf[Function1[List[Any], Any]](evalArgs) //args.tail)
+              }
+            }
+            else throw Error(s"Function ${fn} not defined")
+          case x @ _ => throw Error(s"$x [a literal value] cannot be used as a function")
+        }
+      case x @ _ => x
+    }
+  val DefaultEnvironment = Map[String, Any](
+      // TODO: These are int only stubs, should promote then evaluate
+      scope += "+"      -> { (xs : List[Any]) => xs.foldLeft(0)(_ + _.asInstanceOf[Int]) },
+      scope += "-"      -> { (xs : List[Any]) => xs.tail.foldLeft(xs.head.asInstanceOf[Int])(_ - _.asInstanceOf[Int]) },
+
+      scope += "cons"   -> { (xs : List[Any]) => xs match { 
+                              case a :: (b : List[Any]) :: Nil => a :: b
+                              case e @ _ => throw Error(s"cons called with illegal args: $e") }
+        },
+      scope += "list"   -> { (xs : List[Any]) => List(xs : _*) },
+      scope += "rest"   -> { (xs : List[Any]) => xs match { 
+                              case (a : List[Any]) :: Nil => a.tail
+                              case e @ _ => println(s"ERROR: $e"); throw Error("rest") }
+        },
+      scope += "print"  -> { (xs : List[Any]) => println(Console.YELLOW + xs.foldLeft("")(_ + _))
+    )
+}
 
 // exceptions
 case class Error(e : String) extends Exception(e)
@@ -31,91 +88,68 @@ object Scope {
   }
 }
 
-// Expressions
-trait Expr { def eval(scope : Scope) : Any }
-case class LiteralFloat(value : Float) extends Expr { def eval(scope : Scope) = value }
-case class LiteralInt(value : Int) extends Expr { def eval(scope : Scope) = value }
-case class LiteralString(value : String) extends Expr { def eval(scope : Scope) = value }
-case class Symbol(name : String) extends Expr { 
-  def eval(scope : Scope) = scope(name)
-}
-case class S(args : List[Expr]) extends Expr { 
-  def eval(scope : Scope) = {
-    args match {
-      case Symbol("lambda") :: S(fargs) :: body =>
-        val symargs = fargs.map { x => assert(x.isInstanceOf[Symbol], s"lambda argument $x is not a string!"); x.asInstanceOf[Symbol] }
-
-        { (xs : List[Any]) =>
-          val newScope = Scope(scope, newBindings = (symargs.map(_.name) zip xs) : _*)
-          for (l <- body.dropRight(1)) l.eval(newScope)
-          body.last.eval(newScope)
-        }
-      // MACRO
-      // case Symbol("macro") :: Symbol(name) :: S(margs) :: body =>
-      case Symbol("lambda") :: rest => throw Error(s"malformed lambda expression (lambda ${rest})")
-      case (Symbol("set!") | Symbol("def")) :: Symbol(name) :: value :: Nil =>
-        val res = value.eval(scope)
-        scope += name -> res
-        res
-      case Symbol(fn) :: rest => // Function call
-        if (scope isDefinedAt fn) {
-          val evalArgs = args.tail.map(_.eval(scope))
-          scope(fn).asInstanceOf[Function1[List[Any], Any]](evalArgs) //args.tail)
-        }
-        else throw Error(s"Function ${fn} not defined")
-      case x @ _ => throw Error(s"$x [a literal value] cannot be used as a function")
-    }
-  }
-}
+// Macro class
+case class Macro(fn : List[Any] => Any)
 
 // Reader
 class Reader extends RegexParsers {
   override val whiteSpace = """([\t ]*(?<!\\);[^\n\r$]+|[\t ]+)""".r
-  def float  = ("""[-+]?\d+\.\d+""".r | ("""[-+]?\d+""" <~ "f")) ^^ { x => LiteralFloat(x.toFloat) }
-  def int    = """[-+]?\d+""".r ^^ { x => LiteralInt(x.toInt) }
-  def string = """(?<!\\)".*?(?<!\\)"""".r ^^ { x => LiteralString(x.drop(1).dropRight(1)) }
+  def float  = ("""[-+]?\d+\.\d+""".r | ("""[-+]?\d+""" <~ "f")) ^^ { x => x.toFloat }
+  def int    = """[-+]?\d+""".r ^^ { x => x.toInt }
+  def string = """(?<!\\)".*?(?<!\\)"""".r ^^ { x => x.drop(1).dropRight(1) }
   def symbol = """[^\d()\s][^\s()]*""".r ^^ { x => Symbol(x) }
-  def sexpr  : Parser[Expr] = "(" ~> rep1(expr) <~ ")" ^^ { x => S(x) }
+  def sexpr  : Parser[Any] = "(" ~> rep1(expr) <~ ")"
   def expr   = (float | int | string | symbol | sexpr)
-  def apply(input: String) : Expr = parseAll(expr, input) match {
+  def apply(input: String) : Any = parseAll(expr, input) match {
       case Success(result, _) => result
       case failure : NoSuccess => throw ParseError(failure.msg)
     }
 }
 
-// Tests
-implicit class X(a : Any) {
-  def shouldBe(b : Any) = {
-    val exprString = a.toString + " == " + b.toString
-    var shortened  = exprString.substring(0, math.min(exprString.length, 97))
-    if (shortened.length < exprString.length) shortened = shortened + "..."
-    if (a == b) println(Console.GREEN + f"$shortened%-100s ${"[OK]"}%6s")
-    else println(Console.RED + f"$shortened%-100s ${"[FAIL]"}%6s")
+object Tests {
+  // Tests
+  implicit class X(a : Any) {
+    def shouldBe(b : Any) = {
+      val exprString = a.toString + " == " + b.toString
+      var shortened  = exprString.substring(0, math.min(exprString.length, 97))
+      if (shortened.length < exprString.length) shortened = shortened + "..."
+      if (a == b) println(Console.GREEN + f"$shortened%-100s ${"[OK]"}%6s")
+      else println(Console.RED + f"$shortened%-100s ${"[FAIL]"}%6s")
+    }
+  }
+
+  def main(args : Array[String]) {
+    val reader = new Reader
+    val scope  = Scope(DefaultEnvironment : _*)
+
+    scope += "function" -> { (xs : List[Any]) => xs.foldLeft(0.0f)(_ + _.asInstanceOf[Float]) },
+
+    reader("function") shouldBe Symbol("function")
+    reader(""""function"""") shouldBe "function"
+    reader("1") shouldBe 1
+    reader("2.2") shouldBe 2.2f
+    reader("(2)") shouldBe List(2)
+    reader("(2.2)") shouldBe List(2.2f)
+    reader("(2.2 3)") shouldBe List(2.2f, 3)
+    reader("(2.2 3 function)") shouldBe List(2.2f, 3, Symbol("function"))
+    reader("(function 2.2)") shouldBe List(Symbol("function"), 2.2f)
+    reader("(lambda (x) (+ x 1))") shouldBe List(Symbol("lambda"), List(Symbol("x")), List(Symbol("+"), Symbol("x"), 1))
+    eval(reader("(function 2.2 3.3 (function 1.0 2.0))"), scope) shouldBe 8.5f
+    eval(reader("(def incr (lambda (x) (+ x 1)))"), scope).toString shouldBe "<function1>"
+    eval(reader("(set! y 4)"), scope) shouldBe 4
+    eval(reader("(def add-y (lambda (x) (+ x y)))"), scope).toString shouldBe "<function1>"
+    eval(reader("(incr 1)"), scope) shouldBe 2
+    eval(reader("(incr 4)"), scope) shouldBe 5
+    eval(reader("(add-y 4)"), scope) shouldBe 8
+    eval(reader("(add-y 1)"), scope) shouldBe 5
+    eval(reader("(macro test (x) (cons (quote -) (rest x)))"), scope).toString shouldBe "Macro(<function1>)"
+    eval(reader("(quote (1 2 3))"), scope) shouldBe List(1, 2, 3)
+    eval(reader("(test (+ 1 4))"), scope) shouldBe -3
+    eval(reader("(quote (+ 1 4))"), scope) shouldBe List('+, 1, 4)
+    eval(reader("(macro m (x) (println x))"), scope).toString shouldBe "Macro(<function1>)"
+    eval(reader("(m (+ 1 3))"), scope) shouldBe Unit
+    eval(reader("(macro define (name args body) (list (quote def) name (list (quote lambda) args body)))"), scope).toString shouldBe "Macro(<function1>)"
+    eval(reader("(define addxy (x y) (+ x y))"), scope).toString shouldBe "<function1>"
+    eval(reader("(addxy 1 3)"), scope) shouldBe 4
   }
 }
-val reader = new Reader
-val scope = Scope()
-
-// TODO these require static types
-scope += "function" -> { (xs : List[Any]) => xs.foldLeft(0.0f)(_ + _.asInstanceOf[Float]) }
-scope += "+" -> { (xs : List[Any]) => xs.foldLeft(0)(_ + _.asInstanceOf[Int]) }
-
-reader("function") shouldBe Symbol("function")
-reader(""""function"""") shouldBe LiteralString("function")
-reader("1") shouldBe LiteralInt(1)
-reader("2.2") shouldBe LiteralFloat(2.2f)
-reader("(2)") shouldBe S(List(LiteralInt(2)))
-reader("(2.2)") shouldBe S(List(LiteralFloat(2.2f)))
-reader("(2.2 3)") shouldBe S(List(LiteralFloat(2.2f), LiteralInt(3)))
-reader("(2.2 3 function)") shouldBe S(List(LiteralFloat(2.2f), LiteralInt(3), Symbol("function")))
-reader("(function 2.2)") shouldBe S(List(Symbol("function"), LiteralFloat(2.2f)))
-reader("(lambda (x) (+ x 1))") shouldBe S(List(Symbol("lambda"), S(List(Symbol("x"))), S(List(Symbol("+"), Symbol("x"), LiteralInt(1)))))
-reader("(function 2.2 3.3 (function 1.0 2.0))").eval(scope) shouldBe 8.5f
-reader("(def incr (lambda (x) (+ x 1)))").eval(scope).toString shouldBe "<function1>"
-reader("(set! y 4)").eval(scope) shouldBe 4
-reader("(def add-y (lambda (x) (+ x y)))").eval(scope).toString shouldBe "<function1>"
-reader("(incr 1)").eval(scope) shouldBe 2
-reader("(incr 4)").eval(scope) shouldBe 5
-reader("(add-y 4)").eval(scope) shouldBe 8
-reader("(add-y 1)").eval(scope) shouldBe 5
-
